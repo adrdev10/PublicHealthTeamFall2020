5d0b85b51233763c91974f09720be89df06ae6cc
Thamare <saintlouisthamare@gmail.com>
2020-11-14 15:54:51 -0500

    my cod33e

--- lib/screens/resourcespage.dart
+++ lib/screens/resourcespage.dart
@@ -4,54 +4,103 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Resourcespage extends StatelessWidget{
-@override
-State<StatefulWidget> createState (){
-    return resourcespageState();
-}
-class resourcespageState extends State <resourcespage>{
-@override
-Widget build (Buildcontext context){
-   return Scaffold(
-      body: Container (
-         decoration: BoxDecoration(  
-            gradient: LinearGradient( 
-                colors: [
-                 Colors.blue,
-                 Colors.purple 
+class Resourcespage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+      colors: [Colors.blue, Colors.purple],
+      begin: Alignment.bottomCenter,
+      end: Alignment.topRight,
+    )
+        ),
+
+            //   child: Container(
+            //      decoration: new BoxDecoration(
+            //          image: new DecorationImage(
+            //               return Image(image: AssetImage('assets/clip-1072 2.png'));
+            //              fit: BoxFit.fill,
+            //          )
+            //      )
+
+            //  )
+    width: MediaQuery.of(context).size.width,
+    child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+      Container(
+        Text("RESOURCES",
+              style: TextStyle(
+              fontSize: 35,
+              color: Colors.white,
+              fontWeight: FontWeight.w500)), 
+      ]),
+      
+           Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 130.0,
+                    height: 40,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: Color(0xff300EB5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: Text('FLORIDA'),
+                      textColor: Color(0xffffffff),
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  ButtonTheme(
+                    minWidth: 130.0,
+                    height: 40,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: Color(0xff520382),
+                      child: Text('US'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      textColor: Color(0xffffffff),
+                    ),
+                  ),
                 ],
-                begin:Alignment.bottomCenter,
-                end: Alignment.topRight,
-      )     
-              )
-        child: Container(
-           alignment: Alignment.topRight,
-              child: Text(
-                  'HEKA';
-               style: TextStyle(
-               fontSize: 40,
-               color: Colors.white,
-               )
-              )
-     child: Container(
-         decoration: new BoxDecoration(
-             image: new DecorationImage(
-                  return Image(image: AssetImage('assets/clip-1072 2.png'));  
-                 fit: BoxFit.fill,
-             )
-         )
-           
-     )
+              ),
+
+
+
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Container(
+    padding: EdgeInsets.all(15),
+    decoration: new BoxDecoration(
+      image: new Image.asset(clip-1072 2.png),
+     fit: BoxFit.fill,
+    ),
+    mainAxisAlignment: Maint
+    Container(
+        Text("HEKA",
+              style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.w500)),
+        )
+      ],)
+   
+
+      )
         
         )
-           
-            
         
-          
-
-}
-
-}
-
-
+           
+                  
+              
+               ),
+  }
+  
 }
