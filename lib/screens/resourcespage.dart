d3bcd7f6ea6d2ca9fddeb13ff8b789c882126eff
Thamare <saintlouisthamare@gmail.com>
2020-11-18 20:54:19 -0500

    my codeeeeee

--- lib/screens/resourcespage.dart
+++ lib/screens/resourcespage.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -28,16 +28,11 @@ class Resourcespage extends StatelessWidget {
               fontSize: 35,
               color: Colors.white,
               fontWeight: FontWeight.w500)),
-      )],
-              
-      Row(
+      ),
+ Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                      padding: EdgeInsets.all(15),
-                      decoration: new BoxDecoration(
-                      image: new Image(clip-1072 2.png),
+                       new Image(clip-1072 2.png),
                       
                   )
                   child: Column(
@@ -51,6 +46,15 @@ class Resourcespage extends StatelessWidget {
       ), 
                   
                   ]),
+//  width: MediaQuery.of(context).size.width,
+    
+     Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+      children:<Widget> [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .05,     
+              ),
+      
 
                      Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -59,14 +63,21 @@ class Resourcespage extends StatelessWidget {
                     minWidth: 110.0,
                     height: 20,
                     child: RaisedButton(
-                      onPressed: () {},
+
+                    // I am using this one now to insert the url but I am not too sure if it is the right one
+                      onPressed:_launchURL,
                       color: Color(0xff300EB5),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(5.0),
                       ),
-                      child: Link(path),    // I will place the link right there but i am not too sure how to do that
-
-                     
+                      _launchURL() async {    //This is where the code for the url is placed
+                            const url = 'https://flutter.dev';
+                                if (await canLaunch(url)) {
+                                    await launch(url);
+                                  } else {
+                                    throw 'Could not launch $url';
+                                         }
+                      } 
                     ),
                   ),
                   SizedBox(width: 5),
@@ -74,16 +85,20 @@ class Resourcespage extends StatelessWidget {
                     minWidth: 130.0,
                     height: 40,
                     child: RaisedButton(
-                      onPressed: () {},
+                      onPressed:_launchUrl,
                       color: Color(0xff520382),
-                      child: Link(),
+                      child: new InkWell(
+              child: new Text('Open Browser'),
+              onTap: () => launch('https://docs.flutter.io/flutter/services/UrlLauncher-class.html')
+                      ),
+             ),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10.0),
                       ),
                       textColor: Color(0xffffffff),
                     ),
-                  ),
-                ],
+                ]),
+                
               ),   
                       
                     
